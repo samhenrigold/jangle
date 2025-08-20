d8295ab20893af113a8e303b914d7030fa180a4c
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2025-08-20 14:32:26 -0400

    legacy CSS/JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   vite: {
-    build: {
-      target: 'es2015',
-      minify: false
-    }
+    plugins: [
+      // @ts-ignore
+      legacy({
+        targets: ['ios_saf >= 4'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      })
+    ]
   }
 });
